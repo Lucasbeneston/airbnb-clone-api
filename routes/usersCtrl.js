6f676337d51a35ae6be77759d73b3123722ba10a
Lucas Beneston <benestonlucas@gmail.com>
2020-07-03 15:57:18 +0200

    POST Place 90%

--- routes/usersCtrl.js
+++ routes/usersCtrl.js
@@ -5,7 +5,7 @@ const models = require('../models');
 
 // Routes
 module.exports = {
-  register: (req, res) => {
+  register: async (req, res) => {
     const user = {
       firstName: req.body.firstName,
       lastName: req.body.lastName,
@@ -21,78 +21,74 @@ module.exports = {
     }
 
     // Faire regEx pour le mail !!!
-    models.Users.findOne({
+    const userFound = await models.Users.findOne({
       attributes: ['email'],
       where: { email: user.email },
-    })
-
-      .then((userFound) => {
-        if (!userFound) {
-          bcrypt.hash(user.password, 5, (err, bcryptedPassword) => {
-            const newUsers = models.Users.create({
+    });
+    try {
+      if (!userFound) {
+        try {
+          bcrypt.hash(user.password, 5, async (err, bcryptedPassword) => {
+            const newUsers = await models.Users.create({
               firstName: user.firstName,
               lastName: user.lastName,
               email: user.email,
               password: bcryptedPassword,
               role: user.role,
-            })
-              .then((newUsers) => {
-                return res.status(201).json({
-                  role: newUsers.role,
-                  userId: newUsers.id,
-                  firstName: newUsers.firstName,
-                  lastName: newUsers.lastName,
-                  email: newUsers.email,
-                });
-              })
-              .catch((err) => {
-                return res.status(500).json({ error: "Ne peut pas ajouter d'utilisateur" });
-              });
+            });
+
+            return res.status(201).json({
+              role: newUsers.role,
+              userId: newUsers.id,
+              firstName: newUsers.firstName,
+              lastName: newUsers.lastName,
+              email: newUsers.email,
+            });
           });
-        } else {
-          return res
-            .status(409)
-            .json({ error: 'Un utilisateur utilisant cette adresse email est déjà enregistré' });
+        } catch (error) {
+          return res.status(500).json({ error: "Ne peut pas ajouter d'utilisateur" });
         }
-      })
-      .catch((err) => {
-        return res.status(500).json({ error: "Impossible de vérifier l'utilisateur" });
-      });
+      } else {
+        return res
+          .status(409)
+          .json({ error: 'Un utilisateur utilisant cette adresse email est déjà enregistré' });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: "Impossible de vérifier l'utilisateur" });
+    }
   },
 
-  login: (req, res) => {
+  login: async (req, res) => {
     const { email, password } = req.body;
 
     if (email == null || password == null) {
       return res.status(400).json({ error: 'Veuillez remplir tous les champs' });
     }
 
-    // A FAIRE : vérifier mail (regex) & la longueur du password
-    models.Users.findOne({
+    const userFound = await models.Users.findOne({
       where: { email },
-    })
-      .then((userFound) => {
-        if (userFound) {
-          bcrypt.compare(password, userFound.password, (errBycrypt, resBycrypt) => {
-            if (resBycrypt) {
-              return res.status(200).json({
-                token: jwtUtils.generateTokenForUser(userFound),
-                user: {
-                  role: userFound.role,
-                  firstName: userFound.firstName,
-                  lastName: userFound.lastName,
-                  email: userFound.email,
-                },
-              });
-            }
-            return res.status(403).json({ error: "Votre mot de passe n'est pas correct" });
-          });
-        } else {
-          return res.status(404).json({ error: "L'utilisateur est introuvable" });
-        }
-      })
-      .catch((err) => {
-        return res.status(500).json({ error: "Impossible de vérifier l'user" });
-      });
+    });
+    try {
+      if (userFound) {
+        bcrypt.compare(password, userFound.password, (errBycrypt, resBycrypt) => {
+          if (resBycrypt) {
+            return res.status(200).json({
+              token: jwtUtils.generateTokenForUser(userFound),
+              user: {
+                role: userFound.role,
+                firstName: userFound.firstName,
+                lastName: userFound.lastName,
+                email: userFound.email,
+              },
+            });
+          }
+          return res.status(403).json({ error: "Votre mot de passe n'est pas correct" });
+        });
+      } else {
+        return res.status(404).json({ error: "L'utilisateur est introuvable" });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: "Impossible de vérifier l'user" });
+    }
   },
 };
